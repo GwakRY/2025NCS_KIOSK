312c48a02cdbca78585ff9c1b7e69cfbf89fee0a
GwakRaeYeong <raeyeong383@gmail.com>
2025-04-08 19:39:55 +0900

    week06 v2.7

--- week06.py
+++ week06.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     menu_prices = [2000, 3000, 4900, 3300]
 
     menu = kiosk.Menu(menu_drinks, menu_prices)
-    order_processor = kiosk.OrderProcessor(menu) #has a,aggregation
+    order_processor = kiosk.OrderProcessor(menu) #<has a> relation,aggregation
     order_processor.run()
 
